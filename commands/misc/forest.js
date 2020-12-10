f26bdcbcc19d841e91450c80fd5ed51668da1810
unknown <goyalaryan51@gmail.com>
2020-12-11 01:52:04 +0530

    first commit

--- commands/misc/forest.js
+++ commands/misc/forest.js
@@ -43,7 +43,7 @@ const mention = message.author
           .setTimestamp()
           .setFooter(`Planter: ${message.author.username} `)
           .addFields(
-              { name: `If you wish to join them in the forest app click on the link below or copy the code and paste it in your Forest app. `},
+              { name: `If you wish to join them in the forest app click on the link below or copy the code and paste it in your Forest app. ` , inline: true },
               { name: '\u200B', value: 'Forest info:' },
               { name: `Use code: ${arguments[0]} or click this link: https://www.forestapp.cc/join-room?token=${arguments[0]}.`, value: '\u200B' },
               { name: ':stopwatch: Duration:', value: `${arguments[1]} min`, inline: true },
