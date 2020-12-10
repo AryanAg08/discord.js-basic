04cb934cc7b587f799f58dda3a564e42b0f18e0c
unknown <goyalaryan51@gmail.com>
2020-12-10 22:44:12 +0530

    first commit

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 //  const config = require('./config.json')
 
+const poll = require('./poll')
+
 client.on('ready', () => {
     console.log('The bot is online!')
 }) 
+
+poll(client)
 
 client.login(process.env.DJS_TOKEN)
