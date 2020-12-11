ba30ffab49a2e118e12733ff8b2b937f5a4823ce
unknown <goyalaryan51@gmail.com>
2020-12-12 02:40:31 +0530

    first commit

--- commands/misc/serverinfo.js
+++ commands/misc/serverinfo.js
@@ -32,7 +32,9 @@ module.exports = {
         {
           name: 'Afk Timeout (in min.)', 
           value: afkTimeout / 60,
-        })
+        },
+        { name: ` This server is for Studying purpose only. Feel free to ask anything. We all want the happy and the productive environment. I hop you all help in creating such environment. Looking forward to see you all studying and Happy. :smile: ` ,  value: '\u200B' },
+        )
       
         message.channel.send(embed)
     },
